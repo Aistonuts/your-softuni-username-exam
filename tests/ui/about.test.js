d50ffa9bac808aa5a272bb07cde21be0b0983371
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2025-03-10 09:16:05 +0200

    Update about.test.js

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
-  await page.goto('http://localhost:8080/about');
+  await page.goto('https://your-softuni-username-exam-3y1y.onrender.com/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');

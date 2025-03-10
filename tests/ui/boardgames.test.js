d5b45e714ad825254d05ce83dd62d2a977a6e167
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2025-03-10 09:16:34 +0200

    Update boardgames.test.js

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://your-softuni-username-exam-3y1y.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
